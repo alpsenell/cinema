0d42d87666062a282fb99feec386f5ed2a3501ea
alpsenell <alp@useinsider.com>
2019-12-16 00:00:28 +0300

    try to add axios

---
+++ src/components/MainPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        {{ info }}
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+
+}
+</script>
+
+
+<style scoped lang="scss">
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+//import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
 
 new Vue({
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+        .get('https://raw.githubusercontent.com/alpsenell/cinema/master/data/movielist.json')
+        .then(response => (this.info = response))
+  },
   render: h => h(App),
 }).$mount('#app')

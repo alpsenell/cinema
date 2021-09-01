a1e602f2b628c5ba92c94fd3c8415ea3b62f6bef
alpsenell <alp@useinsider.com>
2021-09-02 00:05:12 +0300

    SD-667 | test

--- .github/actions/automated-version/index.js
+++ .github/actions/automated-version/index.js
@@ -86,11 +86,13 @@ Toolkit.run(async (tools) => {
         newVersion = execSync(`npm version --git-tag-version=false ${version}`).toString().trim().replace(/^v/, '');
         console.log(`::set-output name=newTag::${newVersion}`);
 
-        const remoteRepo = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`;
+        // const remoteRepo = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`;
 
         await tools.runInWorkspace('git', ['tag', newVersion]);
-        await tools.runInWorkspace('git', ['push', remoteRepo, '--follow-tags']);
-        await tools.runInWorkspace('git', ['push', remoteRepo, '--tags']);
+        await tools.runInWorkspace('git', ['push', '--follow-tags']);
+        await tools.runInWorkspace('git', ['push', 'tags']);
+        // await tools.runInWorkspace('git', ['push', remoteRepo, '--follow-tags']);
+        // await tools.runInWorkspace('git', ['push', remoteRepo, '--tags']);
     } catch (e) {
         tools.log.fatal(e);
         tools.exit.failure('Failed to bump version');
